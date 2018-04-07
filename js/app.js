6a25585941ef7fb7c43f26c20797ab40ba109d76
Kareem Mostafa <kareemmmostafaa@gmail.com>
2018-04-08 00:05:32 +0200

    Add main JS variables

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
-
+let cards        = document.querySelectorAll(".card");
+let holdCards    = [...cards];
+let deck         = document.querySelector(".deck");
+let movesCounter = 0;
+let ratingStars  = 0;
+let clicks       = 0;
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
